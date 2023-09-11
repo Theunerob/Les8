db3c452638453fdfc3821de6d1d1dcc5bdc24b53
Theunerob <81445217+Theunerob@users.noreply.github.com>
2023-09-11 21:56:01 +0200

    reboot laptop test

--- NHA-Les9/fouten.py
+++ NHA-Les9/fouten.py
@@ -2,8 +2,23 @@ import math
 
 def discriminant(a,b,c):
     D1 = (-b + math.sqrt(b**2 - 4*a*c))/(2 * a)
-    D2 = (-b - math.sqrt(b**2 - 4*a*c))/(2 * a)
+    D2 = (-b - math.sqrt(b**2 - 4*a*c))/(2 * a)  
+
     uitvoer = list[D1,D2]
     return(uitvoer)
 
-print(discriminant(4,3,3))
+A = int(input("Geef waarde A"))
+B = int(input("Geef waarde B"))
+C = int(input("Geef waarde C"))
+
+# print(type(A1))
+
+uitkomst = discriminant(A,B,C)
+
+print(uitkomst)
+
+D1 = uitkomst[0]
+D2 = uitkomst[1]
+
+print(f"De discriminant(en) voor {a}x^2 + {b}x + {c} zijn :")
+print(f"{D1} en {D2}")
